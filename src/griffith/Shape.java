f4888ab880b4590312214e87f698225ff36dad6d
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-04 19:52:29 +0000

    Implemented Shape class

--- src/griffith/Shape.java
+++ src/griffith/Shape.java
@@ -2,4 +2,29 @@ package griffith;
 
 public abstract class Shape {
 
+    private String name;
+    // Constructor 
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // NO implementation
+    public abstract double area();
+    
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
