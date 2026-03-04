dd4d414cf78bbe06779f4e8690882bedc21fea7e
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-04 20:03:06 +0000

    Implementing Rhombus  class

--- src/griffith/Rhombus.java
+++ src/griffith/Rhombus.java
@@ -1,9 +1,42 @@
+// Mohamed Abdikadir Noor
+// 3141112
+
 package griffith;
 
-public class Rhombus {
+public class Rhombus extends Shape {
 
-	public Rhombus() {
-		// TODO Auto-generated constructor stub
-	}
+    private double side;
+    private double height;
+    // Constructor
+    public Rhombus(String name, double side, double height) {
+        super(name);
+        this.side = side;
+        this.height = height;
+    }
+    // Getter
+    public double getSide() {
+        return side;
+    }
+    // Getter
+    public double getHeight() {
+        return height;
+    }
+    
+    // No setter because it is treated as immutable objects
 
+    // overriding the parent class
+    @Override
+    public double area() {
+        return 0;   
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;   
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Side: " + side + " Height: " + height;
+    }
 }
