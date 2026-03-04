4df93d0f143750c86e6a1fa7fe48b5f1643f2d99
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-04 20:16:04 +0000

    testing Rhombus

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 class ShapeTest {
 
+	// Circle
+	
 	@Test
 	public void testCircleArea() {
 	    Circle c = new Circle("Circle", 3.0);
@@ -25,6 +27,27 @@ class ShapeTest {
 	public void testCircleToString() {
 	    Circle c = new Circle("Circle", 3.0);
 	    assertTrue(c.toString().contains("Radius: 3.0"));
+	}
+
+	// Rhombus
+	
+	
+	@Test
+	public void testRhombusArea() {
+	    Rhombus r = new Rhombus("Rhombus", 4, 3);
+	    assertEquals(12, r.area(), 0.1);
+	}
+
+	@Test
+	public void testRhombusPerimeter() {
+	    Rhombus r = new Rhombus("Rhombus", 4, 3);
+	    assertEquals(16, r.perimeter(), 0.1);
+	}
+
+	@Test
+	public void testRhombusToString() {
+	    Rhombus r = new Rhombus("Rhombus", 4, 3);
+	    assertTrue(r.toString().contains("Side: 4.0"));
 	}
 
 }
