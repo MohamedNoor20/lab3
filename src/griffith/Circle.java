8b5282ac0f403e8688708ff2f48132a7087665b1
MohamedNoor20 <mohammedamiin200@gmail.com>
2026-03-04 19:59:12 +0000

    Implementing Circle class

--- src/griffith/Circle.java
+++ src/griffith/Circle.java
@@ -1,9 +1,37 @@
+// Mohamed Abdikadir Noor
+// 3141112
+
 package griffith;
 
-public class Circle {
+public class Circle extends Shape {
 
-	public Circle() {
-		// TODO Auto-generated constructor stub
-	}
+    private double radius;
+    // Constructor
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+    // Getter
+    public double getRadius() {
+        return radius;
+    }
+    // Setter
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    // overriding the parent class
+    @Override
+    public double area() {
+        return 0;  
+    }
 
+    @Override
+    public double perimeter() {
+        return 0;   
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Radius: " + radius;
+    }
 }
